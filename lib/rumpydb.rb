1d32e8792dd2ca41751e9e7e463aaf60e1f8ba5c
Sergio Rafael Gianazza <sgianazza@gmail.com>
2012-12-12 00:09:14 -0300

    Added basic find_all support

--- lib/rumpydb.rb
+++ lib/rumpydb.rb
@@ -5,7 +5,7 @@ require 'pry'
 
 class RumpyDB
   RUMPYDB_SEPARATOR="---EOO"
-  
+
   class << self
     attr_accessor :id, :file_db
   end
@@ -15,17 +15,23 @@ class RumpyDB
     @file_db = opts[:file_db]
     @id = 0
   end
-  
+
+  def find_all
+    objects = IO.readlines(@file_db, RUMPYDB_SEPARATOR)
+    objects.map do |row|
+      row =~ Regexp.new("\\[\\d+\\]\(.+)---EOO", Regexp::MULTILINE)
+      deserialize($1)
+    end.compact
+  end
+
   def find(id)
-    #objects = open_db('r').read
-    #Marshal.load(objects[objects.size - id])
     objects = IO.readlines(@file_db, RUMPYDB_SEPARATOR)
     finded = objects.collect do |row|
       row =~ Regexp.new("\\[#{id}\\]\(.+)---EOO", Regexp::MULTILINE)
       $1
     end.compact
 
-    return deserialize(finded.first) if finded.size == 1 
+    return deserialize(finded.first) if finded.size == 1
 
     raise "RumpyDB::NotFound" if finded.size == 0
     raise "RumpyDB::DirtyId" if finded.size > 1
@@ -38,7 +44,7 @@ class RumpyDB
     end
     @id
   end
-  
+
   private
   def serialize(id,object)
     "[#{id}]#{Marshal.dump(object).dump}---EOO"
@@ -51,7 +57,7 @@ class RumpyDB
   def open_db(opts='r', &block)
     file_open = File.open(@file_db,opts+":UTF-8")
     if block_given?
-      block.call(file_open) 
+      block.call(file_open)
       file_open.close
     end
     file_open
--- test/rumpydb_test.rb
+++ test/rumpydb_test.rb
@@ -4,6 +4,10 @@ require 'pry'
 
 class TestObject
   attr_accessor :name
+
+  def ==(other)
+    self.name == other.name
+  end
 end
 
 describe RumpyDB do
@@ -54,7 +58,6 @@ describe RumpyDB do
     test = TestObject.new
     test.name = "Gianux"
     id = @rumpydb.save(test)
-    puts id
     returned = @rumpydb.find(id)
     returned.name.must_equal test.name
   end
@@ -71,6 +74,46 @@ describe RumpyDB do
     object2 = @rumpydb.find(id2)
     object1.name.must_equal test1.name
     object2.name.must_equal test2.name
+  end
+
+  describe "#find_all" do
+    it "returns all the objects when there are only one object" do
+      test1 = TestObject.new
+      test1.name = "Gianu"
+      @rumpydb.save(test1)
+
+      all = @rumpydb.find_all
+
+      all.size.must_equal 1
+      all[0].name.must_equal test1.name
+    end
+
+    it "returns all the objects when there are two objects" do
+      test1 = TestObject.new
+      test1.name = "Gianu"
+      @rumpydb.save(test1)
+
+      test2 = TestObject.new
+      test2.name = "Tute"
+      @rumpydb.save(test2)
+
+      all = @rumpydb.find_all
+
+      all.size.must_equal 2
+      all.must_include test1
+      all.must_include test2
+    end
+
+    it "returns all the objects when there are multiple objects" do
+     10.times do |i|
+       test = TestObject.new
+       test.name = "Gianu#{i}"
+       @rumpydb.save(test)
+     end
+
+     all = @rumpydb.find_all
+     all.size.must_equal 10
+    end
   end
 
 end
